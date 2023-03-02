37dc0bda81caeed999bd732ed361656fe1fe2c4c
chrisMartinezHm <chris.martinez.hm@gmail.com>
2023-03-01 20:14:33 -0500

    Map Update

--- src/components/mapComponent.jsx
+++ src/components/mapComponent.jsx
@@ -1,29 +1,26 @@
 import React, { Component } from "react";
 import { MapContainer, TileLayer, useMapEvents } from "react-leaflet";
-import { L } from "leaflet";
-import icon from "./constants";
+// import { L } from "leaflet";
 import { MAP_ZOOM_LEVEL, COORDS } from "../config";
 import "../css/style.css";
 
-function Map() {
+function MapEventHandler(showForm) {
   const map = useMapEvents({
     click: (e) => {
-      const { lat, lng } = e.latlng;
-      L.marker([lat, lng], { icon }).addTo(map);
+      console.log(this.props);
     },
   });
+  return null;
+}
 
+function Map() {
   return (
-    <MapContainer
-      onClick={this.props.onShowForm}
-      center={COORDS}
-      zoom={MAP_ZOOM_LEVEL}
-      scrollWheelZoom={true}
-    >
+    <MapContainer center={COORDS} zoom={MAP_ZOOM_LEVEL} scrollWheelZoom={true}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
+      <MapEventHandler onShowForm={() => console.log("TEST - Map")} />
     </MapContainer>
   );
 }
