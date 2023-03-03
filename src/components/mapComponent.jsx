2695839cd3aa21f84c43e1cf1cd92a250f917871
chrisMartinezHm <chris.martinez.hm@gmail.com>
2023-03-02 21:18:36 -0500

    Map Update

--- src/components/mapComponent.jsx
+++ src/components/mapComponent.jsx
@@ -1,3 +1,4 @@
+import { Icon } from "leaflet";
 import React, { Component, useState } from "react";
 import {
   MapContainer,
@@ -10,8 +11,10 @@ import {
 import { MAP_ZOOM_LEVEL, COORDS } from "../config";
 import "../css/style.css";
 
-const markers = [];
-let coords = [];
+const customIcon = new Icon({
+  iconUrl: require("../img/marker-icon.png"),
+  iconSize: [38, 38],
+});
 
 function MapEventHandler({ onRenderMarker }) {
   const [markers, setMarkers] = useState([]);
@@ -30,7 +33,7 @@ function MapEventHandler({ onRenderMarker }) {
   });
 
   return markers.map((marker) => (
-    <Marker key={marker.key} position={marker.coords}>
+    <Marker key={marker.key} position={marker.coords} icon={customIcon}>
       <Popup>Hello</Popup>
     </Marker>
   ));
@@ -45,9 +48,20 @@ class Map extends Component {
           zoom={MAP_ZOOM_LEVEL}
           scrollWheelZoom={true}
         >
-          <TileLayer
+          {/* <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          /> */}
+          {/* Jawg.dark */}
+          {/* <TileLayer
+            attribution='<a href="http:url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" //jawg.io" title="Tiles Courtesy of Jawg Maps" target="_blank">&copy; <b>Jawg</b>Maps</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.jawg.io/jawg-dark/{z}/{x}/{y}{r}.png?access-token={accessToken}"
+            accessToken="oAWFbGge6rAM4G7XaDxi34ZkmDxc7QSadqOBBGci1RGjwmZ49Yd2zibsUCsHGl1j"
+          /> */}
+          <TileLayer
+            attribution='<a href="http://jawg.io" title="Tiles Courtesy of Jawg Maps" target="_blank">&copy; <b>Jawg</b>Maps</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.jawg.io/jawg-light/{z}/{x}/{y}{r}.png?access-token={accessToken}"
+            accessToken="oAWFbGge6rAM4G7XaDxi34ZkmDxc7QSadqOBBGci1RGjwmZ49Yd2zibsUCsHGl1j"
           />
           <MapEventHandler />
         </MapContainer>
